b6e00170848665f64878f3ac3eb15e1cee04dfd5
AnnaDoroshenko <annadoroshenko03@gmail.com>
2019-10-13 19:26:04 +0300

    added signs check, semicolon check

--- main.cpp
+++ main.cpp
@@ -9,29 +9,30 @@
 #include <unordered_map>
 
 
-enum TokenType {INTEGER, FLOAT, VARIABLE, ADDITION, SUBTRACTION, MODULO, MULTIPLICATION,
-    DIVISION, SEMICOLON, ASSIGNMENT, LEFT_BRACE, RIGHT_BRACE};
+enum class TokenType {INTEGER, FLOAT, VARIABLE, ADDITION, SUBTRACTION, MODULO,
+    MULTIPLICATION, DIVISION, SEMICOLON, ASSIGNMENT, LEFT_BRACE, RIGHT_BRACE};
 
 
-std::ostream& operator<<(std::ostream& os, TokenType type)
-{
+std::ostream& operator<<(std::ostream& os, TokenType type) {
     switch(type)
     {
-        case INTEGER : os << "integer"; break;
-        case FLOAT : os << "float"; break;
-        case VARIABLE : os << "variable"; break;
-        case ADDITION : os << "addition"; break;
-        case SUBTRACTION : os << "subtraction"; break;
-        case MODULO : os << "modulo"; break;
-        case MULTIPLICATION : os << "multiplication"; break;
-        case DIVISION : os << "division"; break;
-        case SEMICOLON : os << "semicolon"; break;
-        case ASSIGNMENT : os << "assignment"; break;
-        case LEFT_BRACE : os << "left brace"; break;
-        case RIGHT_BRACE : os << "right brace"; break;
+        case TokenType::INTEGER : os << "integer"; break;
+        case TokenType::FLOAT : os << "float"; break;
+        case TokenType::VARIABLE : os << "variable"; break;
+        case TokenType::ADDITION : os << "addition"; break;
+        case TokenType::SUBTRACTION : os << "subtraction"; break;
+        case TokenType::MODULO : os << "modulo"; break;
+        case TokenType::MULTIPLICATION : os << "multiplication"; break;
+        case TokenType::DIVISION : os << "division"; break;
+        case TokenType::SEMICOLON : os << "semicolon"; break;
+        case TokenType::ASSIGNMENT : os << "assignment"; break;
+        case TokenType::LEFT_BRACE : os << "left brace"; break;
+        case TokenType::RIGHT_BRACE : os << "right brace"; break;
     }
+
     return os;
 }
+
 
 struct Token {
     private:
@@ -40,32 +41,30 @@ struct Token {
         unsigned int position;
 
     public:
-        Token(std::string value, TokenType type, unsigned int position) {
-            this->value = value;
-            this->type = type;
-            this->position = position;
-        };
+        Token(const std::string& value, TokenType type, unsigned int position)
+                : value(value), type(type), position(position) {}
 
-        std::string getValue() {
-            return this->value;
+        std::string getValue() const {
+            return value;
         }
 
-        TokenType getType() {
-            return this->type;
+        TokenType getType() const {
+            return type;
         }
 
-        unsigned int getPosition() {
-            return this->position;
+        unsigned int getPosition() const {
+            return position;
         }
 
-        friend std::ostream& operator<<(std::ostream& os, const Token& token) {
-            os << token.value << " - " << token.type << " [" << token.position << "]";
+        std::ostream& operator<<(std::ostream& os) {
+            os << value << " - " << type << " [" << position << "]";
             return os;
         }
 };
 
 
-bool isToken (const char& ch) {
+
+bool isToken (char ch) {
     return ((ch == '=') || (ch == '+') || (ch == '-') || (ch == '%') ||
             (ch == '*') || (ch == '/') || (ch == '(') || (ch == ')') ||
             (ch == ';') || (ch == ' ') || (ch == '\n'));
@@ -73,7 +72,7 @@ bool isToken (const char& ch) {
 
 
 std::vector<Token> getTokens (const std::string& input) {
-    unsigned int inputSize = input.size();
+    const unsigned int inputSize = input.size();
     std::vector<Token> tokens;
 
     const auto pushToken = [&tokens](char c, TokenType type, unsigned int pos) {
@@ -87,12 +86,12 @@ std::vector<Token> getTokens (const std::string& input) {
         else if (currentChar == '%') pushToken(currentChar, TokenType::MODULO, i);
         else if (currentChar == '*') pushToken(currentChar, TokenType::MULTIPLICATION, i);
         else if (currentChar == '/') pushToken(currentChar, TokenType::DIVISION, i);
-        else if (currentChar == '(') pushToken(currentChar, TokenType::RIGHT_BRACE, i);
-        else if (currentChar == ')') pushToken(currentChar, TokenType::LEFT_BRACE, i);
+        else if (currentChar == '(') pushToken(currentChar, TokenType::LEFT_BRACE, i);
+        else if (currentChar == ')') pushToken(currentChar, TokenType::RIGHT_BRACE, i);
         else if (currentChar == ';') pushToken(currentChar, TokenType::SEMICOLON, i);
         else if ((currentChar == ' ') || (currentChar == '\n')) continue;
         else if (isalpha(currentChar)) {
-            std::string var = std::string(1, currentChar);
+            std::string var(1, currentChar);
             unsigned int count = 0;
             for (unsigned int pos = i; pos < inputSize; pos++) {
                 if ((pos+1 == inputSize) || isToken(input[pos+1])) {
@@ -111,11 +110,11 @@ std::vector<Token> getTokens (const std::string& input) {
             i += count;
         } else if (isdigit(currentChar)) {
             bool isDot = false;
-            std::string num = std::string(1, currentChar);
+            std::string num(1, currentChar);
             unsigned int count = 0;
             for (unsigned int pos = i; pos < inputSize; pos++) {
                 if ((pos+1 == inputSize) || isToken(input[pos+1])) {
-                    TokenType currType = (isDot) ? TokenType::FLOAT : TokenType::INTEGER;
+                    TokenType currType = isDot ? TokenType::FLOAT : TokenType::INTEGER;
                     tokens.push_back({num, currType, pos-count});
                     break;
                 } else if (isdigit(input[pos+1])) {
@@ -124,20 +123,26 @@ std::vector<Token> getTokens (const std::string& input) {
                 } else if (input[pos+1] == '.') {
                     if (!isDot) isDot = true;
                     else {
-                        std::cerr << "Invalid number format at " << pos-count << std::endl;
-                        exit(0);
+                        std::cerr << "Invalid number format at ["
+                            << pos-count << "]" << std::endl;
+                        exit(-1);
                     }
                     num.push_back(input[pos+1]);
                     count++;
                 } else {
                     num.push_back(input[pos+1]);
                     count++;
-                    std::cerr << "Invalid number format at " << pos+1-count << std::endl;
-                    exit(0);
+                    std::cerr << "Invalid number format at ["
+                        << pos+1-count << "]" << std::endl;
+                    exit(-1);
                 }
             }
             i += count;
         }
+    }
+
+    if (tokens.back().getType() != TokenType::SEMICOLON) {
+        std::cout << "Missed semicolon in the end of sequence" << std::endl;
     }
 
 
@@ -146,35 +151,38 @@ std::vector<Token> getTokens (const std::string& input) {
 
 
 void checkBraces (const std::vector<Token>& tokens) {
-    std::vector<Token> braces;
-    for (unsigned int token = 0; token < tokens.size(); token++) {
-        Token currentToken = tokens.at(token);
-        if (currentToken.getValue() == "(") braces.push_back(currentToken);
+    using Position = unsigned int;
+    std::vector<Position> braces;
+    for (const auto& currentToken : tokens) {
+        if (currentToken.getValue() == "(") braces.push_back(currentToken.getPosition());
         else if (currentToken.getValue() == ")") {
-            if (!braces.empty()) braces.pop_back();
-            else {
-                std::cerr << "Missed left brace for [" << currentToken.getPosition() << "]" << std::endl;
-                exit(0);
+            if (braces.empty()) {
+                std::cerr << "Missed left brace for ["
+                    << currentToken.getPosition() << "]" << std::endl;
+                exit(-1);
             }
+            braces.pop_back();
         }
     }
     if (!braces.empty()) {
-        std::cerr << "Missed right brace for [" << braces.back().getPosition() << "]" << std::endl;
+        std::cerr << "Missed right brace for ["
+            << braces.back() << "]" << std::endl;
         exit(0);
     }
 }
 
 
-typedef std::vector<TokenType> TokenList;
+using TokenList = std::vector<TokenType>;
 
-bool isValid(std::unordered_map<TokenType, TokenList>& tokenMap, const TokenType& current, const TokenType& previous){
-    std::vector<TokenType> validTokens = tokenMap[current];
-    return (std::find(validTokens.begin(), validTokens.end(), previous));
+bool isValid(std::unordered_map<TokenType, TokenList>& tokenMap,
+        const TokenType& current, const TokenType& previous) {
+    const std::vector<TokenType>& validTokens = tokenMap[current];
+    return (std::find(validTokens.cbegin(), validTokens.cend(), previous)
+            != validTokens.cend());
 }
 
 
 void checkOrder (const std::vector<Token>& tokens) {
-    /* typedef std::vector<TokenType> TokenList; */
     std::unordered_map<TokenType, TokenList> tokenMap;
     tokenMap[TokenType::INTEGER] = {TokenType::ADDITION, TokenType::SUBTRACTION,
         TokenType::MODULO, TokenType::MULTIPLICATION, TokenType::DIVISION,
@@ -187,10 +195,10 @@ void checkOrder (const std::vector<Token>& tokens) {
         TokenType::ASSIGNMENT, TokenType::LEFT_BRACE};
     tokenMap[TokenType::ADDITION] = {TokenType::INTEGER, TokenType::FLOAT,
         TokenType::VARIABLE, TokenType::ADDITION, TokenType::SUBTRACTION,
-        TokenType::LEFT_BRACE, TokenType::LEFT_BRACE};
+        TokenType::LEFT_BRACE, TokenType::RIGHT_BRACE};
     tokenMap[TokenType::SUBTRACTION] = {TokenType::INTEGER, TokenType::FLOAT,
         TokenType::VARIABLE, TokenType::ADDITION, TokenType::SUBTRACTION,
-        TokenType::LEFT_BRACE, TokenType::LEFT_BRACE};
+        TokenType::LEFT_BRACE, TokenType::RIGHT_BRACE};
     tokenMap[TokenType::MODULO] = {TokenType::INTEGER, TokenType::FLOAT,
         TokenType::VARIABLE, TokenType::RIGHT_BRACE};
     tokenMap[TokenType::MULTIPLICATION] = {TokenType::INTEGER, TokenType::FLOAT,
@@ -211,12 +219,47 @@ void checkOrder (const std::vector<Token>& tokens) {
     for (unsigned int i = 1; i < tokens.size(); i++) {
         Token currentToken = tokens.at(i);
         TokenType currentType = currentToken.getType();
-        if (!isValid(currentType, previousType)) {
-            std::cerr << "Invalid token after token at [" << previousToken.getPosition() << "]" << std::endl;
+        if (!isValid(tokenMap, currentType, previousType)) {
+            std::cerr << "Invalid type " << currentType << " after " << previousType
+                << " at [" << previousToken.getPosition() << "]" << std::endl;
             exit(0);
         }
         previousToken = currentToken;
+        previousType = currentType;
     }
+}
+
+
+bool checkSigns(const std::vector<Token>& tokens) {
+    if (tokens.empty()) return true;
+
+    const auto firstType = tokens[0].getType();
+    if (firstType == TokenType::MULTIPLICATION ||
+            firstType == TokenType::DIVISION ||
+            firstType == TokenType::MODULO) {
+        std::cout << "Invalid first token" << std::endl;
+        return false;
+    }
+
+    bool begin = true;
+    int count = 0;
+    for (unsigned int i = 0; i < tokens.size() - 1; i++) {
+        const auto type = tokens[i].getType();
+        if (type == TokenType::ADDITION ||
+                type == TokenType::SUBTRACTION ||
+                type == TokenType::MULTIPLICATION ||
+                type == TokenType::DIVISION) {
+            count++;
+        } else count = 0;
+
+        if ((begin && count > 1) || (count > 2)) {
+            std::cout << "Too many signs in sequence!" << std::endl;
+            return false;
+        }
+        if (i == 1) begin = false;
+    }
+
+    return true;
 }
 
 
@@ -225,8 +268,8 @@ int main() {
     std::stringstream ss;
     ss << input.rdbuf();
 
-    // std::cout << ss.str() << std::endl;
     std::string str = ss.str();
+    std::cout << "Input: " << str << std::endl;
     std::vector tokens = getTokens(str);
 
     /* for (unsigned int t = 0; t < tokens.size(); t++) { */
@@ -234,6 +277,9 @@ int main() {
     /* } */
 
     checkBraces(tokens);
+    checkOrder(tokens);
+    checkSigns(tokens);
+
 
     return 0;
 }
